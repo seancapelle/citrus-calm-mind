81904e601ca8e8900837349819e57346d8f3a2ab
Sean Capelle <seancapelle@gmail.com>
2020-03-29 11:27:40 -0400

    Added Doxy

---
+++ script.js
@@ -0,0 +1,16 @@
+$( ".cross" ).hide();
+
+$( ".hamburger" ).click(function() {
+	$( ".menu" ).slideToggle( "slow", function() {
+		$( ".hamburger" ).hide();
+		$( ".cross" ).show();
+	});
+});
+
+$( ".cross" ).click(function() {
+	$( ".menu" ).slideToggle( "slow", function() {
+		$( ".cross" ).hide();
+		$( ".hamburger" ).show();
+	});
+});
+
